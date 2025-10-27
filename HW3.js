e49bdbe6de4319defc1ced1899ea8c4ff5b5f58b
yhatukai <yhatukai@gmail.com>
2025-10-27 17:33:34 +0200

    Add files via upload

---
+++ HW3.js
@@ -0,0 +1,10 @@
+//Heraldo Ghattas
+//Yousef Hatukai
+
+const arr = [0,1,0,2,0,3,0,4]
+let zeroCounter = 0;
+for(i=0;i<arr.length;i++)
+{
+  arr[i] == 0 ? zeroCounter++ : zeroCounter = zeroCounter
+}
+console.log(zeroCounter);
